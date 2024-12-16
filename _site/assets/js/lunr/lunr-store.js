ff98e1fd6d9d4fd30d3955c3f13c29b458c05a48
ihyunu <bylhw1234@gmail.com>
2024-12-17 04:45:12 +0900

    hide

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "사무자동화산업기사 필기 합격 후기",
+        "excerpt":"사무자동화산업기사 필기 합격했습니다.   ","categories": ["자격증"],
+        "tags": ["사무자동화산업기사","사자산기","OA","컴활"],
+        "url": "/%EC%9E%90%EA%B2%A9%EC%A6%9D/%EC%82%AC%EB%AC%B4%EC%9E%90%EB%8F%99%ED%99%94%EC%82%B0%EC%97%85%EA%B8%B0%EC%82%AC-%ED%95%84%EA%B8%B0-%ED%95%A9%EA%B2%A9/",
+        "teaser": null
+      },{
+        "title": "Adsp 취득기념 후기",
+        "excerpt":"Adsp 취득에 성공했습니다.   ","categories": ["자격증"],
+        "tags": ["Adsp","데이터분석","BA"],
+        "url": "/%EC%9E%90%EA%B2%A9%EC%A6%9D/Adsp/",
+        "teaser": null
+      },{
+        "title": "SQLD 취득기념 후기",
+        "excerpt":"sqld 취득에 성공했습니다.   ","categories": ["자격증"],
+        "tags": ["SQLD","데이터베이스"],
+        "url": "/%EC%9E%90%EA%B2%A9%EC%A6%9D/SQLD/",
+        "teaser": null
+      }]
